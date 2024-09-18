45d2926dae6fc395f9fbf3b9c3538a6390fa3ee7
Aaqif Shafi <63208372+aaqifshafi@users.noreply.github.com>
2024-09-18 17:57:06 +0530

    Refactor Footer component layout and add contact information

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,11 @@
 function Footer() {
-  return <div className="">
-    <div className="container mx-auto p-2 bg-white w-[600px] text-slate-700 flex justify-between">
-      <div className="flex flex-col gap-2">
-        <h5 className="underline underline-offset-4">Info@devlane.com</h5>
-        <p className="text-[8px] font-bold text-slate-500">Srinagar,J&K</p>
-      </div>
+  return (
+    <div className="">
+      <div className="container mx-auto p-2 bg-white w-[600px] text-slate-700 flex justify-between">
+        <div className="flex flex-col gap-2">
+          <h5 className="underline underline-offset-4">Info@devlane.com</h5>
+          <p className="text-[8px] font-bold text-slate-500">Srinagar,J&K</p>
+        </div>
 
         <div className="flex flex-col gap-2 items-end">
           <div className="flex gap-3 h-[24px]">

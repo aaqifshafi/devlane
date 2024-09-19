6f2878d18c558c56e0d0b50723252d78ea720c18
Aaqif Shafi <63208372+aaqifshafi@users.noreply.github.com>
2024-09-19 21:22:11 +0530

    Refactor Main component to improve animation and layout

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,31 +1,45 @@
 "use client";
+import React from "react";
 import { motion } from "framer-motion";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import Hero from "@/app/components/Hero";
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
+const circularRevealVariants = {
+  hidden: {
+    clipPath: "ellipse(50% 0% at 50% 100%)",
     opacity: 1,
-    transition: { duration: 0.5, when: "beforeChildren" },
+  },
+  visible: {
+    clipPath: "ellipse(100% 50% at 50% 50%)",
+    opacity: 0.3,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut",
+    },
   },
 };
 
-const overlayVariants = {
-  hidden: { y: 0 },
-  visible: { y: "-100%", transition: { duration: 0.7, ease: "easeInOut" } },
+const contentVariants = {
+  hidden: { y: "100%", opacity: 0 },
+  visible: {
+    y: "0%",
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
 };
 
-const contentVariants = {
+const childVariants = {
   hidden: { opacity: 0, y: 20 },
   visible: {
     opacity: 1,
     y: 0,
-    transition: {
-      delay: 0.7,
-      duration: 0.5,
-    },
+    transition: { duration: 1, ease: "easeOut" },
   },
 };
 
@@ -34,21 +48,27 @@ const Main = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      variants={containerVariants}
       className="relative w-full h-screen overflow-y-auto scrollbar-hide"
     >
+      <div className="absolute inset-0 bg-background" />
       <motion.div
-        variants={overlayVariants}
-        className="absolute top-0 left-0 w-full h-full bg-transition z-10"
+        variants={circularRevealVariants}
+        className="absolute inset-0 z-10  bg-transition"
       />
 
       <motion.div
         variants={contentVariants}
-        className="relative z-20 w-full h-full"
+        className="absolute inset-0 z-20 bg-background"
       >
-        <Header />
-        <Hero />
-        <Footer />
+        <motion.div variants={childVariants}>
+          <Header />
+        </motion.div>
+        <motion.div variants={childVariants}>
+          <Hero />
+        </motion.div>
+        <motion.div variants={childVariants}>
+          <Footer />
+        </motion.div>
       </motion.div>
     </motion.div>
   );

5162c710481d5eb59427e2941360237eb4d8b143
faaridzargar <zargarfaarid@gmail.com>
2024-09-20 20:59:30 +0530

    Add- Blob

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,14 +1,23 @@
 import { Typography } from "@mui/material";
+import Blob from "./Blob";
 
 function Hero() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <span className="font-semi-bold text-center text-4xl">
-        Design Develop & Deploy with
-      </span>
-      <Typography className="font-extrabold" variant="h2" component="div">
-        devlane.
-      </Typography>
+    <div className="relative h-screen flex flex-col justify-center items-center">
+      <Blob />
+      {/* Content */}
+      <div className="z-10 text-center">
+        <span className="text-white font-semi-bold text-4xl transition-transform transform hover:scale-105">
+          Design Develop & Deploy with
+        </span>
+        <Typography
+          className="font-extrabold transition-transform transform hover:scale-105"
+          variant="h2"
+          component="div"
+        >
+          devlane.
+        </Typography>
+      </div>
     </div>
   );
 }

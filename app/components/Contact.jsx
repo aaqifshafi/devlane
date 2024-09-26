6c5d646ff83c3c7bcaa7d6d55cc0f11d45830ddf
faaridzargar <zargarfaarid@gmail.com>
2024-09-26 17:08:10 +0530

    Refactor-Contact

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import RippleEffect from './animations/RippleEffect';
 
 function Contact() {
   return (
-    <div className='mx-auto my-12'>
-      <div className='text-black size-[22rem] rounded-full bg-transition flex flex-col justify-center items-center hover:text-white transition ease-in-out delay-50 hover:scale-125 hover:bg-black'>
+      <div className='mx-auto my-12 cursor-pointer'>
+    <RippleEffect className="inline-block rounded-full font-semibold">
+      <div className='text-black size-[22rem] rounded-full bg-slate-200 flex flex-col justify-center items-center hover:text-white hover:bg-transparent'>
         <p className='text-sm text-center'>Click to</p>
-        <h1 className='text-3xl text-center font-semi-bold transition-transform hover:text-slate-200'>
+        <h1 className='text-3xl text-center font-semi-bold'>
           Get in touch
         </h1>
       </div>
+    </RippleEffect>
     </div>
   );
 }
